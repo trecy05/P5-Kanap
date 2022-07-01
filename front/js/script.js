b836b4e284bde4c581ca21d10dedbc2a6c6aa13e
trecy <trecym@laposte.net>
2022-07-01 11:37:00 +0200

    récupération des données de l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,5 @@
-const fetchKanap = async () => {
-  await fetch("http://localhost:3000/api/products").then((res) =>
-    console.log(res.json())
-  );
+//Recuperation des articles sur l'API
+const theArticles = async () => {
+  let recoveryArticles = await fetch("http://localhost:3000/api/products");
+  return await recoveryArticles.json();
 };
-fetchKanap();
