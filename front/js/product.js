e1f7c9043bb60b61c46e3c9ccde12a958e18e812
trecy <trecym@laposte.net>
2022-07-21 12:12:00 +0200

    modification de ma fonction async

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,18 @@
-const idElement = window.location.search.split("?").join("");
+const queryString = window.location.href;
+const usp = new URL(queryString);
+const idElement = usp.searchParams.get("id");
+
+console.log(idElement);
 
 // Recuperation d'un element dans l'API
 const getElement = async () => {
-  await fetch(`http://localhost:3000/api/products/${idElement}`).then((rep) => {
-    return res.json();
-  });
+  await fetch(`http://localhost:3000/api/products/${idElement}`)
+    .then((rep) => {
+      return rep.json();
+    })
+    .then(async (repAPI) => {
+      element = await repAPI;
+      console.table(element);
+    });
 };
-console.log(idElement);
+getElement();
